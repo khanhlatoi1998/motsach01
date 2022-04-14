d320ab4af4f9287339bf980ba6aa20ce19701adf
khanhlatoi1998 <khanhlatoi1998@gmail.com>
2022-04-14 22:49:08 +0700

    update

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,12 +3,18 @@
 const Validator = (options) => {
     const formElement = document.querySelector(options.form);
 
-    const validate = (e, rule, inputElement) => {
-        let target = e.target;
-        let value = target.value;
+    const getParent = (element, selector) => {
+        while (element.parentElement) {
+            if (element.parentElement.matches(selector)) {
+                return element.parentElement;
+            }
+            element = element.parentElement;
+        }
+    };
+
+    const validate = (value, rule, inputElement) => {
         const messageInput = rule.test(value); 
-        const messageElement = inputElement.parentElement.querySelector(options.errorSelector);
-        
+        const messageElement = getParent(inputElement, options.formGroupSelector).querySelector(options.errorSelector);
         if (messageInput === false) {
             inputElement.classList.add('input-err');
             messageElement.style.display = "block";
@@ -19,17 +25,44 @@ const Validator = (options) => {
             inputElement.classList.remove('input-err');
             messageElement.style.display = "none";
         }
+        return messageInput;
     };
 
     if (formElement) {
+        // submit form
+        formElement.addEventListener('submit', (e) => { 
+            e.preventDefault();
+            let isFormValidate = true;
+            options.rules.forEach((rule) => {
+                let inputElement = formElement.querySelector(rule.selector);
+                let newValidate = validate(inputElement.value, rule, inputElement);
+                if (inputElement)
+                    newValidate;
+                if (newValidate === false) {
+                    isFormValidate = false;
+                }
+            });
+
+            if (isFormValidate === true) {
+                if (typeof options.onSubmit === 'function') {
+                    const enabelInputs = formElement.querySelectorAll('[name]');
+                    const formValues = Array.from(enabelInputs).reduce((value, item) => {
+                        value[item.name] = item.value;
+                        return value;
+                    }, {});
+                    options.onSubmit(formValues);
+                }
+            }
+        });
+
         options.rules.forEach((rule) => {
-            const inputElement = formElement.querySelector(rule.selector);
+            let inputElement = formElement.querySelector(rule.selector);
             if (inputElement) {
                 inputElement.addEventListener("blur", (e) => {
-                    validate(e, rule, inputElement);
+                    validate(inputElement.value, rule, inputElement);
                 });
                 inputElement.addEventListener("input", (e) => {
-                    validate(e, rule, inputElement);
+                    validate(inputElement.value, rule, inputElement);
                 });
             }
         })
@@ -39,7 +72,7 @@ const Validator = (options) => {
 Validator.isRequest = (selector) => {
     return {
         selector : selector,
-        test: (value) => {
+        test: (value) => { 
             return value.trim() ? true : false;
         }
     };
@@ -55,41 +88,41 @@ Validator.isEmail = (selector) => {
     };
 };
 
-Validator.isPassword = (selector) => {
+Validator.minLength = (selector, min) => {
     return {
-        selector: selector,
-        test: (value) => {
-            return value.trim() ? true : false;
+        selector : selector,
+        test: (value) => { 
+            return value.trim() && value.length >= min ? true : false;
         }
     };
 };
 
-Validator.isPhone = (selector) => {
-    return {
-        selector: selector,
-        test: (value) => {
-            return value.trim() ? true : false;
-        }
-    };
-};
-
-Validator.isDate = (selector) => {
-    return {
-        selector: selector,
-        test: (value) => {
-            return value.trim() ? true : false;
-        }
-    };
-};
+// Validator.isConfirmed = (selector, getConfirmValule) => {
+//     return {
+//         selector: selector,
+//         test: (value) => {
+//             return value.trim() && value === getConfirmValule() ? true : false;
+//         }
+//     };
+// };
 
 Validator({
     form: "#contact__form",
     errorSelector: '.form-message', 
+    formGroupSelector: '.form__control',
     rules: [
         Validator.isRequest("#name"),
+        Validator.minLength("#email", 12),
         Validator.isEmail("#email"),
-        Validator.isRequest("#password"),
+        Validator.minLength("#password", 6),
+        // Validator.isConfirmed("#repeat-password", () => {
+        //    return document.querySelector("#contact__form #password").value ; 
+        // }),
         Validator.isRequest("#phone"),
         Validator.isRequest("#date"),
-    ]
+        Validator.isRequest("#gender"),
+    ],
+    onSubmit: (date) => {
+        console.log(date);
+    }
 });
